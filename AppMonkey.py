cb11b21366eda3eabd4353a52460372e1aeaa70f
ted.wang <tedwang@sina.cn>
2019-04-14 21:09:15 +0800

    0.1

--- AppMonkey.py
+++ AppMonkey.py
@@ -11,6 +11,9 @@ class MyThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.cpu = 0
+        self.acpu = 0
+        self.amem = 0
+        self.mem = 0
 
     def run(self):
         p = os.popen('adb shell top -m 1 -n 1 -t | grep System')
@@ -18,41 +21,92 @@ class MyThread(threading.Thread):
         b = a.split(',')
         user = b[0].strip().split(' ')[1].replace('%', '')
         system = b[1].strip().split(' ')[1].replace('%', '')
-        self.cpu = int(user) + int(system)
+        self.cpu = float(user) + float(system)
+        print(self.cpu)
 
-    def get_res(self):
+        p2 = os.popen('adb shell dumpsys meminfo|grep  -e calculator -e "Total RAM"')
+        a2 = p2.readline().strip().split()
+        self.amem = a2[0].replace(',', '').replace('K:', '')
+        p2.readline()
+        b2 = p2.readline().strip().split()[2].replace(',', '').replace('K', '')
+        self.mem = int(b2)
+
+        print(self.amem)
+        p1 = os.popen('adb shell dumpsys cpuinfo |grep  calculator')
+        a1 = p1.read().strip()
+        b1 = a1.split(' ')[0]
+        self.acpu = float(b1.strip().replace('%', ''))
+        print(self.acpu)
+
+    def get_cpu(self):
         return self.cpu
+
+    def get_acpu(self):
+        return self.acpu
+
+    def get_amem(self):
+        return self.amem
+
+    def get_mem(self):
+        return self.mem
 
 
 if __name__ == '__main__':
+    t0 = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
     p = subprocess.Popen('adb shell monkey '
                          '-p com.miui.calculator '
                          '-v -v -v '
+                         '--pct-touch 30 '
+                         '--pct-syskeys 0 '
+                         '--pct-appswitch 0 '
                          '--throttle 100 '
-                         '-s 26 '
-                         # '1>D:\\monkeyinfo.txt 2>D:\\monkeyerror.txt '
-                         '1000')
+                         '-s 25 '
+                         '1>./' + t0 + '_info.txt '
+                         '2>./' + t0 + '_error.txt '
+                         '5000',
+                         shell=True)
     cpu = []
+    acpu = []
+    amem = []
+    mem = 0
     flag = 1
     while flag:
         if p.poll() == 0:
             flag = 0
+        thd = MyThread()
         ti = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         t = ti.split(' ')[1]
-        thd = MyThread()
         thd.start()
         thd.join()
         # cpu.append(str(thd.get_res()))
-        cpu.append([t, thd.get_res()])
+        cpu.append([t, thd.get_cpu()])
+        acpu.append([t, thd.get_acpu()])
+        amem.append([t, thd.get_amem()])
+        mem = thd.get_mem()
+        time.sleep(1)
     print(cpu)
-    html = open('.\\LineGraph.html', mode='r', encoding='utf-8').read()
-    data = ''
+    print(acpu)
+    print(amem)
+    print(mem)
+    html = open('./LineGraph.html', mode='r', encoding='utf-8').read()
+    cpu_data = ''
     for i in cpu:
-        data += '["'+i[0]+'",'+str(i[1])+']'
-        data += ','
-    html = html.replace('%data', data)
+        cpu_data += '["'+i[0]+'",'+str(i[1])+']'
+        cpu_data += ','
+    acpu_data = ''
+    for j in acpu:
+        acpu_data += '["' + j[0] + '",' + str(j[1]) + ']'
+        acpu_data += ','
+    amem_data = ''
+    for k in amem:
+        amem_data += '["' + k[0] + '",' + str(k[1]) + ']'
+        amem_data += ','
+    html = html.replace('%cpu%', cpu_data)
+    html = html.replace('%acpu%', acpu_data)
+    html = html.replace('%amem%', amem_data)
+    html = html.replace('%mem%', str(mem))
     t = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
-    f = open('.\\' + t + '.html', mode='w', encoding='utf-8')
+    f = open('./' + t + '.html', mode='w', encoding='utf-8')
     f.write(html)
     f.close()
 
--- test.py
+++ test.py
@@ -11,27 +11,41 @@ import os, subprocess
 #                  '1>D:\\monkeyinfo.txt 2>D:\\monkeyerror.txt '
 #                  '2000'):
 #     print(1)
-# p = subprocess.Popen('adb shell monkey '
-#                      '-p com.miui.calculator '
-#                      '-v -v -v '
-#                      '--throttle 100 '
-#                      '-s 26 '
-#                      # '1>D:\\monkeyinfo.txt 2>D:\\monkeyerror.txt '
-#                      '100')
+# p = subprocess.Popen('adb shell monkey -p com.miui.calculator -v -v -v --throttle 100 -s 26 100', shell=True)
 # flag = 1
 # while flag:
 #     # print(1)
 #     if p.poll() == 0:
 #         flag = 0
 
-p = os.popen('adb shell top -m 1 -n 1 -t | grep System')
-a = p.read()
-print(a)
-b = a.split(',')
-user = b[0].strip().split(' ')[1].replace('%', '')
-system = b[1].strip().split(' ')[1].replace('%', '')
-print(user)
-print(system)
+# p = os.popen('adb shell top -m 1 -n 1 -t | grep System')
+# a = p.read()
+# print(a)
+# b = a.split(',')
+# user = b[0].strip().split(' ')[1].replace('%', '')
+# system = b[1].strip().split(' ')[1].replace('%', '')
+# print(float(user)+float(system))
+
+p = os.popen('adb shell dumpsys meminfo|grep  -e calculator -e "Total RAM"')
+a = p.readline().strip().split()
+# print(a[0].replace(',', '').replace('K:', ''))
+amem = a[0].replace(',', '').replace('K:', '')
+p.readline()
+t = p.readline().strip().split()[2].replace(',', '').replace('K', '')
+print(t)
+# print(a.split(' ')[5].replace('K:', '').replace(',', ''))
+
+# p1 = os.popen('adb shell dumpsys cpuinfo |grep  calculator')
+# a1 = p1.read().strip()
+# print(a1)
+# b1 = a1.split(' ')[0]
+# print(b1.strip().replace('%', ''))
+
+# p2 = os.popen('adb shell dumpsys meminfo|grep  calculator|head -1')
+# a2 = p2.read().strip()
+# b2 = a2.split(' ')[0]
+# print(b2)
+# print(b2.strip().replace('K:', '').replace(',', ''))
 
 
 
